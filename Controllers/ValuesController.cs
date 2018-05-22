cc54407aaf9323141d430ba3f2d4f13745a3bc2b
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2018-05-23 00:31:59 +0530

    added ValuesController constructor and _context in ValuesController

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DatingApp.API.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DatingApp.API.Controllers
@@ -9,6 +10,12 @@ namespace DatingApp.API.Controllers
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+        private readonly DataContext _context;
+
+        public ValuesController(DataContext context)
+        {
+            this._context = context;
+        }
         // GET api/values
         [HttpGet]
         public IEnumerable<string> Get()
